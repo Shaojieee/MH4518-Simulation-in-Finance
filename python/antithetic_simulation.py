e4442526ab5db665e32bdfa372ef7f950d83b420
keekteng <keekteng@gmail.com>
2022-10-29 17:16:30 +0800

    Changed print statement

--- python/antithetic_simulation.py
+++ python/antithetic_simulation.py
@@ -112,7 +112,7 @@ while date_to_predict<=end_date:
     hist_end+=relativedelta(days=+1)
     trading_days_to_simulate-=1
 
-print("Variance of expected payoff: "+str(np.var(expected_payoff_maturity)))
+print(f'Variance of expected payoff: {np.var(expected_payoff_maturity)} for {Nsim} Simulations.')
 plt_1 = plt.figure(figsize=(30,10))
 plt.plot(predicted_option_price)
 
