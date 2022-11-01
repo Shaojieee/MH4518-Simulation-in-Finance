a60022b39a833b1179acc5fe1ff1832fb72f8708
Shaojieee <shaojieee@gmail.com>
2022-11-01 12:42:01 +0800

    Reformat code

--- python/utils/simulation_function.py
+++ python/utils/simulation_function.py
@@ -1,33 +1,36 @@
 import numpy as np
+import math
 
-def SimMultiGBM(S0,v,sigma,Deltat,T):
-    m=int(T/Deltat)
-    p=len(S0)
-    S=np.zeros((p,m+1))
-    S[:,0]=S0
-    Z = np.random.multivariate_normal(mean=v*Deltat,cov=sigma*Deltat,size=(m,1,1))
-    Z = np.transpose(Z[:,0,0,:])
 
-    for i in range(1,m+1):
-        S[:,i:i+1]=np.exp(np.log(S[:,i-1:i])+Z[:,i-1:i])
+def SimMultiGBM(S0, v, sigma, Deltat, T):
+    m = int(T / Deltat)
+    p = len(S0)
+    S = np.zeros((p, m + 1))
+    S[:, 0] = S0
+    Z = np.random.multivariate_normal(mean=v * Deltat, cov=sigma * Deltat, size=(m, 1, 1))
+    Z = np.transpose(Z[:, 0, 0, :])
+
+    for i in range(1, m + 1):
+        S[:, i:i + 1] = np.exp(np.log(S[:, i - 1:i]) + Z[:, i - 1:i])
 
     return S
 
-def SimMultiGBMAV(S0,v,sigma,Deltat,T):
-    m=int(T/Deltat)
-    p=len(S0)
-    S=np.zeros((p,m+1))
-    Stilde=np.zeros((p,m+1))
-    S[:,0]=S0
-    Stilde[:,0]=S0
+
+def SimMultiGBMAV(S0, v, sigma, Deltat, T):
+    m = int(T / Deltat)
+    p = len(S0)
+    S = np.zeros((p, m + 1))
+    Stilde = np.zeros((p, m + 1))
+    S[:, 0] = S0
+    Stilde[:, 0] = S0
 
     # Applying Linear transform on standard multivariate normal samples
-    Z = np.random.multivariate_normal(mean=np.zeros(len(v)),cov=np.eye(N=len(v),M=len(v)),size=(m,1,1))
-    Z = np.transpose(Z[:,0,0,:])
+    Z = np.random.multivariate_normal(mean=np.zeros(len(v)), cov=np.eye(N=len(v), M=len(v)), size=(m, 1, 1))
+    Z = np.transpose(Z[:, 0, 0, :])
     A = np.linalg.cholesky(sigma)
 
-    for i in range(1,m+1):
-        S[:,i:i+1]=np.exp(np.log(S[:,i-1:i])+np.dot(A,Z[:,i-1:i])+v*Deltat)
-        Stilde[:,i:i+1]=np.exp(np.log(Stilde[:,i-1:i])+np.dot(A,-Z[:,i-1:i])+v*Deltat)
+    for i in range(1, m + 1):
+        S[:, i:i + 1] = np.exp(np.log(S[:, i - 1:i]) + np.dot(A*Deltat, Z[:, i - 1:i]) + (v * Deltat).reshape(3,1))
+        Stilde[:, i:i + 1] = np.exp(np.log(Stilde[:, i - 1:i]) + np.dot(A*Deltat, -Z[:, i - 1:i]) + (v * Deltat).reshape(3,1))
 
-    return S,Stilde
+    return S, Stilde
