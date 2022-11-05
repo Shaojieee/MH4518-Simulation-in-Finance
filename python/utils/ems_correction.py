fcbe43a7dc06e9adbd2891f89f9209fbb6d0681a
Keekteng <keekteng@gmail.com>
2022-11-05 18:02:30 +0800

    commit

--- python/utils/ems_correction.py
+++ python/utils/ems_correction.py
@@ -3,14 +3,14 @@ import numpy as np
 def EMSCorrection(S,Nsim,r,Deltat,T):
 
     m = int(T/Deltat)
-    S_Star = np.zeros((Nsim*2,m+1))
+    S_Star = np.zeros((Nsim,m+1))
     S = np.array(S)
     S_Star[:,0] = S[:,0]
-    Z = np.zeros((Nsim*2,m))
+    Z = np.zeros((Nsim,m))
 
     for i in range(1,m+1):
         Z[:,i-1] = S_Star[:,i-1] * (S[:,i]/S[:,i-1])
-        Z_0 = 1/(Nsim*2) * np.exp(-1*r*(i*Deltat)/T) * np.sum(Z[:,i-1])
+        Z_0 = 1/(Nsim) * np.exp(-1*r*(i*Deltat)/T) * np.sum(Z[:,i-1])
         S_Star[:,i] = S_Star[:,0] * Z[:,i-1]/Z_0
 
     return S_Star.tolist()
